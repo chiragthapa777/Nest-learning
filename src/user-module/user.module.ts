64175ba67717fb80f2d4c531aeacc1a35f522f54
ChiragThapa <chiragThapa777@gmail.com>
2022-03-18 20:34:10 +0545

    middleware, nestmiddleware, user().apply.forRoutes()

--- src/user-module/user.module.ts
+++ src/user-module/user.module.ts
@@ -1,4 +1,5 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
+import { LoggerMiddleware } from "./middleware";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 
@@ -6,4 +7,15 @@ import { UserService } from "./user.service";
     controllers:[UserController],
     providers:[UserService]
 })
-export class UserModule{}
+export class UserModule implements NestModule{
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+        .apply(LoggerMiddleware,)//for other middle wares use comman and include in apply()
+        // .exclude(
+        //     'user/re',
+        //      {}
+        // )
+        // .forRoutes(UserController)
+        .forRoutes("user/try","user/data")
+    }
+}
