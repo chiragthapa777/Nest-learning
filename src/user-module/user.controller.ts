c96bbe58606d1d223c9ca3f18e59e6c0730b40df
ChiragThapa <chiragThapa777@gmail.com>
2022-03-18 15:16:28 +0545

    @Req, @Res, @Query,@Body, @Header, @Params, @Header, @ReDirected ==>refer user controller

--- src/user-module/user.controller.ts
+++ src/user-module/user.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post, Delete, Param, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Post, Delete, Param, UsePipes, ValidationPipe, Req, Query, Headers, HttpCode, Redirect, Res } from "@nestjs/common";
 import { DeleteParamDto, UserDto } from "./dto/user.dto";
 import { User } from "./interface/uesr";//learing interface
 import { UserService } from "./user.service";
+import { Request, Response } from "express";
 
 //route=> /user
 @Controller("user")
@@ -31,5 +32,34 @@ export class UserController{
     deleteUsers(@Param() email:DeleteParamDto):string{
         let users= this.userService.deleteUser(email.email)
         return JSON.stringify(users)
+    }
+
+    @Post("try/:id")
+    tryRoute(
+        @Body() info:{name:string,email:string},
+        @Param() id:string,
+        @Query() q:string,
+        @Req() res:Request,
+        @Headers() headers:{head:string}
+        ){
+            const {head}=headers
+            console.log("body=>", info)
+            console.log("params=>", id)
+            console.log("query", q)
+            console.log("res=>", res.body)
+            console.log("headers=>", head)
+            return "working good job"
+    }
+
+    @Get("re")
+    @Redirect("redirected")
+    redirectTry(){
+        return ""
+    }
+    @Get("redirected")
+    redirectedTry(
+        @Res() res: Response 
+    ){
+        res.status(200).json({msg:"this is good using express res"})
     }
 }
